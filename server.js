21f8d2cd4143fafb0cd457a5673b457273036a3f
Akhil80git <akhilgarg8085@gmail.com>
2026-03-04 08:56:01 +0530

    feat: added basic server and dockerfile setup

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('School Management System API is Running!');
+});
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
